7ed2a8dc4c5f59c9c22a949aafe081e839856552
t-east <azmtomya.a127@gmail.com>
2022-04-08 22:31:38 +0900

    reinit user

--- src/domains/entities/user.go
+++ src/domains/entities/user.go
@@ -1,14 +1,24 @@
 package entities
 
-type User struct {
-	ID       string `json:"id"`
-	Address  string `json:"address"`
-	PubKey   []byte `json:"pub_key"`
-	PrivKey  []byte `json:"priv_key"`
-	Password string `json:"password"`
-}
+import (
+	"time"
 
-type UserRequest struct {
-	Address  string `json:"address"`
-	Password string `json:"password"`
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID             uint           `json:"id"`
+	Name           string         `gorm:"size:256;not null" json:"name"`
+	Email          string         `gorm:"size:256;unique;not null;index" json:"email"`
+	Password       string         `gorm:"not null" json:"password"`
+	HashedPassword string         `gorm:"not null" json:"-"`
+	Icon           string         `json:"icon"`
+	IsActive       bool           `gorm:"default:false;not null" json:"-"`
+	IsSuperUser    bool           `gorm:"default:false;not null" json:"-"`
+	GroupID        uint           `json:"group_id"`
+	Group          Group          `json:"group"`
+	Band           Band           `json:"band"`
+	CreatedAt      time.Time      `json:"-"`
+	UpdatedAt      time.Time      `json:"-"`
+	DeletedAt      gorm.DeletedAt `json:"-"`
 }
